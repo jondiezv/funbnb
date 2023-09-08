1b254aa8b71b86bf50bda94d88b03594f48e920a
Jon <jondiezv@gmail.com>
2023-09-08 19:28:11 -0400

    Fixed seeder files down function

--- backend/db/seeders/02-insert-spots.js
+++ backend/db/seeders/02-insert-spots.js
@@ -3,152 +3,145 @@
 const { Spot } = require("../models");
 
 let options = {};
+options.tableName = "Spots";
 if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA; // define your schema in options object
+  options.schema = process.env.SCHEMA;
 }
 
+const siteSpots = [
+  {
+    ownerId: 1,
+    address: "123 Main St",
+    city: "Orlando",
+    state: "Florida",
+    country: "United States",
+    lat: 40.7128,
+    lng: -74.006,
+    name: "Orlando Lakeside Retreat",
+    description:
+      "A tranquil lakeside retreat located in the heart of Orlando. Perfect for a weekend getaway.",
+    price: 20.5,
+  },
+  {
+    ownerId: 2,
+    address: "456 Another St",
+    city: "San Francisco",
+    state: "California",
+    country: "United States",
+    lat: 37.7749,
+    lng: -122.4194,
+    name: "Golden Gate View Loft",
+    description:
+      "Located in downtown San Francisco, this loft offers a stunning view of the Golden Gate Bridge. Ideal for tourists and business travelers alike.",
+    price: 25.75,
+  },
+  {
+    ownerId: 3,
+    address: "789 Sunset Blvd",
+    city: "Los Angeles",
+    state: "California",
+    country: "United States",
+    lat: 34.0522,
+    lng: -118.2437,
+    name: "LA Oasis",
+    description: "Experience the LA lifestyle.",
+    price: 30.5,
+  },
+  {
+    ownerId: 4,
+    address: "101 Park Ave",
+    city: "New York",
+    state: "New York",
+    country: "United States",
+    lat: 40.7128,
+    lng: -74.006,
+    name: "Central Spot",
+    description: "Close to everything you need in NY.",
+    price: 35.75,
+  },
+  {
+    ownerId: 5,
+    address: "404 Not Found St",
+    city: "San Jose",
+    state: "California",
+    country: "United States",
+    lat: 37.3382,
+    lng: -121.8863,
+    name: "Tech Haven",
+    description: "For those who love technology.",
+    price: 40.0,
+  },
+  {
+    ownerId: 6,
+    address: "600 Commerce St",
+    city: "Dallas",
+    state: "Texas",
+    country: "United States",
+    lat: 32.7767,
+    lng: -96.797,
+    name: "Cowboy Lounge",
+    description: "Experience southern hospitality.",
+    price: 25.0,
+  },
+  {
+    ownerId: 7,
+    address: "700 Lake St",
+    city: "Chicago",
+    state: "Illinois",
+    country: "United States",
+    lat: 41.8781,
+    lng: -87.6298,
+    name: "Windy Haven",
+    description: "In the heart of Chicago.",
+    price: 33.5,
+  },
+  {
+    ownerId: 8,
+    address: "800 Broadway",
+    city: "Nashville",
+    state: "Tennessee",
+    country: "United States",
+    lat: 36.1627,
+    lng: -86.7816,
+    name: "Music City Spot",
+    description: "For the love of music.",
+    price: 22.5,
+  },
+  {
+    ownerId: 1,
+    address: "900 Ocean Dr",
+    city: "Miami",
+    state: "Florida",
+    country: "United States",
+    lat: 25.7617,
+    lng: -80.1918,
+    name: "Beachside Spot",
+    description: "Feel the ocean breeze.",
+    price: 45.0,
+  },
+  {
+    ownerId: 2,
+    address: "1000 Mountain Rd",
+    city: "Denver",
+    state: "Colorado",
+    country: "United States",
+    lat: 39.7392,
+    lng: -104.9903,
+    name: "Rocky Spot",
+    description: "In the mountain range.",
+    price: 35.0,
+  },
+];
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Spot.bulkCreate(
-      [
-        {
-          ownerId: 1,
-          address: "123 Main St",
-          city: "Orlando",
-          state: "Florida",
-          country: "United States",
-          lat: 40.7128,
-          lng: -74.006,
-          name: "Orlando Lakeside Retreat",
-          description:
-            "A tranquil lakeside retreat located in the heart of Orlando. Perfect for a weekend getaway.",
-          price: 20.5,
-        },
-        {
-          ownerId: 2,
-          address: "456 Another St",
-          city: "San Francisco",
-          state: "California",
-          country: "United States",
-          lat: 37.7749,
-          lng: -122.4194,
-          name: "Golden Gate View Loft",
-          description:
-            "Located in downtown San Francisco, this loft offers a stunning view of the Golden Gate Bridge. Ideal for tourists and business travelers alike.",
-          price: 25.75,
-        },
-        {
-          ownerId: 3,
-          address: "789 Sunset Blvd",
-          city: "Los Angeles",
-          state: "California",
-          country: "United States",
-          lat: 34.0522,
-          lng: -118.2437,
-          name: "LA Oasis",
-          description: "Experience the LA lifestyle.",
-          price: 30.5,
-        },
-        {
-          ownerId: 4,
-          address: "101 Park Ave",
-          city: "New York",
-          state: "New York",
-          country: "United States",
-          lat: 40.7128,
-          lng: -74.006,
-          name: "Central Spot",
-          description: "Close to everything you need in NY.",
-          price: 35.75,
-        },
-        {
-          ownerId: 5,
-          address: "404 Not Found St",
-          city: "San Jose",
-          state: "California",
-          country: "United States",
-          lat: 37.3382,
-          lng: -121.8863,
-          name: "Tech Haven",
-          description: "For those who love technology.",
-          price: 40.0,
-        },
-        {
-          ownerId: 6,
-          address: "600 Commerce St",
-          city: "Dallas",
-          state: "Texas",
-          country: "United States",
-          lat: 32.7767,
-          lng: -96.797,
-          name: "Cowboy Lounge",
-          description: "Experience southern hospitality.",
-          price: 25.0,
-        },
-        {
-          ownerId: 7,
-          address: "700 Lake St",
-          city: "Chicago",
-          state: "Illinois",
-          country: "United States",
-          lat: 41.8781,
-          lng: -87.6298,
-          name: "Windy Haven",
-          description: "In the heart of Chicago.",
-          price: 33.5,
-        },
-        {
-          ownerId: 8,
-          address: "800 Broadway",
-          city: "Nashville",
-          state: "Tennessee",
-          country: "United States",
-          lat: 36.1627,
-          lng: -86.7816,
-          name: "Music City Spot",
-          description: "For the love of music.",
-          price: 22.5,
-        },
-        {
-          ownerId: 1,
-          address: "900 Ocean Dr",
-          city: "Miami",
-          state: "Florida",
-          country: "United States",
-          lat: 25.7617,
-          lng: -80.1918,
-          name: "Beachside Spot",
-          description: "Feel the ocean breeze.",
-          price: 45.0,
-        },
-        {
-          ownerId: 2,
-          address: "1000 Mountain Rd",
-          city: "Denver",
-          state: "Colorado",
-          country: "United States",
-          lat: 39.7392,
-          lng: -104.9903,
-          name: "Rocky Spot",
-          description: "In the mountain range.",
-          price: 35.0,
-        },
-      ],
-      { validate: true }
-    );
+    await Spot.bulkCreate(siteSpots, {
+      validate: true,
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Spots";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {
-        ownerId: {
-          [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8],
-        },
-      },
-      {}
-    );
+    await queryInterface.bulkDelete(options);
   },
 };
--- backend/db/seeders/04-insert-reviews.js
+++ backend/db/seeders/04-insert-reviews.js
@@ -1,109 +1,105 @@
 "use strict";
+
 const { Review } = require("../models");
 
 let options = {};
+options.tableName = "Reviews";
 if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA; // define your schema in options object
+  options.schema = process.env.SCHEMA;
 }
 
+const siteReviews = [
+  {
+    spotId: 1,
+    userId: 1,
+    review: "Amazing spot, would love to visit again!",
+    stars: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 2,
+    userId: 2,
+    review: "Nice spot but a bit overcrowded.",
+    stars: 4,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 1,
+    userId: 3,
+    review: "Great view and environment.",
+    stars: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 1,
+    userId: 4,
+    review: "Absolutely stunning scenery!",
+    stars: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 5,
+    userId: 5,
+    review: "Good place, but amenities could be improved.",
+    stars: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 8,
+    userId: 6,
+    review: "Visited again, still an amazing place!",
+    stars: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 4,
+    userId: 7,
+    review: "Good for short stays.",
+    stars: 4,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 2,
+    userId: 8,
+    review: "Best spot for nature lovers.",
+    stars: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 5,
+    userId: 8,
+    review: "A bit expensive for what it offers.",
+    stars: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    spotId: 4,
+    userId: 7,
+    review: "Could be cleaner.",
+    stars: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Review.bulkCreate(
-      [
-        {
-          spotId: 1,
-          userId: 1,
-          review: "Amazing spot, would love to visit again!",
-          stars: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 2,
-          userId: 2,
-          review: "Nice spot but a bit overcrowded.",
-          stars: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 1,
-          userId: 3,
-          review: "Great view and environment.",
-          stars: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 1,
-          userId: 4,
-          review: "Absolutely stunning scenery!",
-          stars: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 5,
-          userId: 5,
-          review: "Good place, but amenities could be improved.",
-          stars: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 8,
-          userId: 6,
-          review: "Visited again, still an amazing place!",
-          stars: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 4,
-          userId: 7,
-          review: "Good for short stays.",
-          stars: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 2,
-          userId: 8,
-          review: "Best spot for nature lovers.",
-          stars: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 5,
-          userId: 8,
-          review: "A bit expensive for what it offers.",
-          stars: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          spotId: 4,
-          userId: 7,
-          review: "Could be cleaner.",
-          stars: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      { validate: true }
-    );
+    await Review.bulkCreate(siteReviews, {
+      validate: true,
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Reviews";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {
-        userId: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8] },
-      },
-      {}
-    );
+    await queryInterface.bulkDelete(options);
   },
 };
