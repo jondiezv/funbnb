58f77300407609d1a0daa2ef9dd68bb7ca7a0088
Jon <jondiezv@gmail.com>
2023-12-20 13:52:00 -0500

    Working on bookings

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,10 +1,16 @@
 import { csrfFetch } from "./csrf";
 
 const GET_USER_BOOKINGS = "bookings/getUserBookings";
+const CREATE_BOOKING = "bookings/createBooking";
 
 export const getUserBookings = (bookings) => ({
   type: GET_USER_BOOKINGS,
   bookings,
+});
+
+export const createBooking = (booking) => ({
+  type: CREATE_BOOKING,
+  booking,
 });
 
 export const fetchUserBookings = () => async (dispatch) => {
@@ -17,6 +23,27 @@ export const fetchUserBookings = () => async (dispatch) => {
   }
 };
 
+export const createNewBooking =
+  (spotId, startDate, endDate) => async (dispatch) => {
+    try {
+      const res = await csrfFetch(`/api/spots/${spotId}/bookings`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ startDate, endDate }),
+      });
+
+      if (res.ok) {
+        const newBooking = await res.json();
+        dispatch(createBooking(newBooking));
+        return newBooking;
+      }
+    } catch (error) {
+      console.error("Error creating booking:", error);
+    }
+  };
+
 const initialState = {
   userBookings: [],
 };
@@ -27,6 +54,11 @@ const bookingsReducer = (state = initialState, action) => {
       return {
         ...state,
         userBookings: action.bookings,
+      };
+    case CREATE_BOOKING:
+      return {
+        ...state,
+        userBookings: [...state.userBookings, action.booking],
       };
     default:
       return state;
